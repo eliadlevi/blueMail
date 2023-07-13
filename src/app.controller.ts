2217cfe18609206da368a1d641c3627eead01636
eliad levi <eliadlevi16@gmail.com>
2023-07-13 20:28:16 +0300

    game controller

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { boardPieceValue } from './types/boardTypes';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/getBoard')
+  public getBoard(): boardPieceValue[][] {
+    return this.appService.getBoard();
+  }
+
+  @Post('/move')
+  public move(@Body() body: { x: number; y: number }): string {
+    return this.appService.move(body.x, body.y);
+  }
+}
